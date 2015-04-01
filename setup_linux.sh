ee2ee60404fb860d0e8f415e135402790a29868a
Adam Thomas <sortoflikechess@gmail.com>
2015-04-01 16:25:41 +0100

    Update setup_linux.sh after its first trial by fire.

--- setup_linux.sh
+++ setup_linux.sh
@@ -3,21 +3,29 @@
 # Remove crap
 sudo apt-get remove unity-lens-shopping unity-scope-musicstores
 
+# Update first
+sudo apt-get update
+sudo apt-get upgrade
+
 # Install packages
 sudo apt-get install git tig python-dev python3-dev build-essential \
 libjpeg-dev libpng12-dev zsh gnome-shell gparted gimp \
 ubuntu-restricted-extras vlc ttf-mscorefonts-installer sqlite ruby \
 quicksynergy postgresql-server-dev-all ack-grep sparkleshare \
 memcached libmemcached-dev nautilus-open-terminal libxml2-dev libxslt-dev \
-tree python-pip virtualenv postgresql-client postgresql postgresql-contrib \
+tree python-pip postgresql-client postgresql postgresql-contrib \
 gnome-tweak-tool nodejs npm ruby ruby-build
 
-sudo pip install virtualenvwrapper
+sudo pip install virtualenv virtualenvwrapper
 sudo npm install -g bower grunt-cli
 
 # Install zsh
+sudo apt-get install zsh
 sudo wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
-sudo chsh -s /bin/zsh
+chsh -s /bin/zsh
+
+# Launch ZSH
+zsh
 
 # Install hub
 cd
@@ -36,7 +44,7 @@ rbenv install 2.1.1
 rbenv global 2.1.1
 eval "$(rbenv init -)"
 gem update --system
-gem install compass
+sudo gem install compass
 rbenv rehash
 
 # Install Heroku Toolbelt
@@ -44,11 +52,12 @@ wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 # Download Sublime Text 3
 # (latest version at time of writing)
-wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3065_amd64.deb
+# (needs actual installation)
+wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_amd64.deb
 
 # Install Powerline fonts
 cd
-git clone git@github.com:Lokaltog/powerline-fonts.git
+git clone https://github.com/Lokaltog/powerline-fonts.git
 sudo sh powerline-fonts/install.sh
 
 # Download git completion scripts and put them in the right place
@@ -57,11 +66,14 @@ wget https://github.com/git/git/raw/master/contrib/completion/git-completion.zsh
 
 # Get scripts directory and wire things up
 cd
-git clone git@github.com:adam-incuna/scripts.git
+git clone https://github.com/adam-incuna/scripts.git
 cd scripts/shell-config-backups
 cp .bash_aliases .zshrc .gitconfig .gitignore ~
 cp -f agnoster.zsh-theme ~/.oh-my-zsh/themes/agnoster.zsh-theme
-# cp -f Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+cp -f Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+
+# Make a work directory
+mkdir ~/Projects
 
 # Restart the shell again
 zsh
@@ -70,10 +82,10 @@ zsh
 echo "Other things to set up:"
 echo "  Chrome"
 echo "  LastPass, AdBlock and GitHub Notifier extensions"
-echo "  SSH key for GitHub"
+echo "  Register an SSH key for GitHub at https://github.com/settings/ssh (instructions at https://help.github.com/articles/generating-ssh-keys/)"
 echo "  Change terminal font to Meslo Powerline so that agnoster renders properly"
 echo "  Setup Charlie's PSQL hack at http://meshy.co.uk/posts/postgresql-without-passwords/"
-echo "  Add SublimeText licence"
+echo "  Add SublimeText licence and extensions"
 echo "  Install and configure GNOME taskbar extension (use Firefox, Chrome doesn't like the installer)"
 echo "  Hoard"
 echo "  Get ~/.pypirc config file from the internal wiki"
