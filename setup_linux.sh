a632ebb0983aaecc4636d21c72a8e6724b1b25ad
Adam Thomas <sortoflikechess@gmail.com>
2017-06-26 14:36:03 +0100

    Update setup_linux.sh

--- setup_linux.sh
+++ setup_linux.sh
@@ -23,7 +23,7 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 sudo pip install virtualenv virtualenvwrapper
-sudo npm install -g grunt-cli
+sudo npm install -g bower grunt-cli
 
 # Install zsh
 sudo apt-get install zsh
