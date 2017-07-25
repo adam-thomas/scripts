2a3f186ff316b70ec8d2836e3bcaf44495a0a330
Adam Thomas <sortoflikechess@gmail.com>
2017-07-25 11:33:29 +0100

    Delete make_superuser.py

--- make_superuser.py
+++
@@ -1,9 +0,0 @@
-### make_superuser.py
-# Used to quickly generate a superuser with relevant data for a project.
-# Run with `python manage.py shell < make_superuser.py`
-
-from users.tests.factories import UserFactory
-a = UserFactory.create(name='admin', email='admin@incuna.com', is_staff=True, is_superuser=True)
-a.set_password('admin')
-a.save()
-exit()
